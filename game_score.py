513cbd8490fbb1b8c13acbf25924f8c77e799228
Geetha P <ramgeetha.tamatam@gmail.com>
2026-02-02 12:58:23 +0530

    Add Game Score Program

---
+++ game_score.py
@@ -0,0 +1,10 @@
+
+player_name = input("Enter player name: ")
+games_played = int(input("Enter number of games played: "))
+total_score = int(input("Enter total score achieved: "))
+Average_score = total_score / games_played
+
+# Output display
+print("\nPlayer:", player_name)
+print("\nGames Played:", games_played)
+print("\nAverage score",Average_score )
